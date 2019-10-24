2c1bf560b3827e3e9c61ae457e5eaa9368fb4251
Jonathan Oakes <jonathanmartynoakes@gmail.com>
2019-10-24 15:49:56 +0100

    adding all blog template files for gatsby blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Eos Apps & Websites Blog`,
+    description: `Tech Tips, Tricks And Walkthroughs For Web Development`,
+    author: `Jonathan Oakes`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,18 +13,27 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdowns`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Eos Apps & Websites.blog`,
+        short_name: `Blog`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/eos-logo.jpg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
---
+++ src/markdown-pages/sample-post.md
@@ -0,0 +1,12 @@
+---
+title: "Sample Post"
+date: "2019-10-24"
+---
+
+### Example Page
+
+This is a sample post. Simply add more markdown pages to the markdown-pages folder to create new posts!
+
+<img src="https://eosappsandwebsites.co.uk/static/media/eos-logo.17c7d794.jpg"
+     alt="Eos Apps & Websites Logo"
+     style="height:250px; width:250px; border-radius:20px;" />
